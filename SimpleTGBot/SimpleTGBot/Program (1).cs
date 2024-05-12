b7d622ca9aa7d7632a84802adba748acf5a6f21a
Alexandra Kufilova <alexahariwer@gmail.com>
2024-05-12 16:58:06 +0300

    сделала бонусное задание

---
+++ SimpleTGBot/SimpleTGBot/Program (1).cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SimpleTGBot;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var botToken = "6540476547:AAH0FWgCKSxT1BiwLVM4FdKm28gXoCz44jI";
+        var logFilePath = "C:\\Users\\Srrgei\\Desktop\\SimpleTGBot\\SimpleTGBot\\log.txt";
+        var dataFilePath = "C:\\Users\\Srrgei\\Desktop\\SimpleTGBot\\SimpleTGBot\\dramas.json";
+        var dramaRecommendationService = new DramaRecommendationService(dataFilePath);
+        var telegramBot = new TelegramBot(botToken, logFilePath, dataFilePath, dramaRecommendationService);
+        await telegramBot.Run();
+    }
+
+}
+
